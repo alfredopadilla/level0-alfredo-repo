98672ec74ff2b04c50bc797118d9bd773e18d53f
League <Level 2>
2018-01-20 11:28:25 -0800

    working on Badgers

--- src/Badgers.java
+++ src/Badgers.java
@@ -2,8 +2,47 @@
 public class Badgers {
 
 	public static void main(String[] args) {
-	for(int i=0; i<10; i=+1);
+		for (int i = 1; i < 10; i++) {
+			System.out.print("Badger ");
+		}
+		 	System.out.println();
+		for (int i = 1; i < 2; i++) {
+			System.out.println("Mushroom ");
+			System.out.println();
+			}
+			for (int j = 1; j < 10; j++) {
+				System.out.print("Badger ");
+			}
+			for (int y = 1; y < 10; y++) {
+				System.out.print("Badger ");
+			}
+			for (int z = 1; z < 2; z++) {
+				System.out.print("Mushroom ");
+			}
+			for(int r=1; r<10; r++) {
+				System.out.print("Badger" );
+			}
+			for (int e = 1; e < 1; e++) {
+				System.out.print("African! ");
+			}
+			for (int w = 1; w < 4; w++) {
+				System.out.println("Snake ");
+			}
+			for (int p = 1; p < 1; p++) {
+				System.out.print("Ooh it's a snake ");
+			}
+			for (int u = 1; u < 10; u++) {
+				System.out.println("Badger ");
+				{
+			}
+			for (int q = 1; q < 2; q++) {
+				System.out.print("Mushroom ");
+				}
+
+			}
+
+		}
 
 	}
 
-}
+
