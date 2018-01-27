f7223e03e850822b72c78181e269eddb45e1178a
alfredopadilla <league@WTS-MM-7.local>
2018-01-27 11:27:58 -0800

    Completing Badgers
    almost finished but have to fix some error, such as adding a to badgers

--- src/Badgers.java
+++ src/Badgers.java
@@ -2,47 +2,59 @@
 public class Badgers {
 
 	public static void main(String[] args) {
-		for (int i = 1; i < 10; i++) {
-			System.out.print("Badger ");
+		String part1 ="Badger,qq ";
+		for (int i = 1; i < 11; i++) {
+			System.out.print(part1);
 		}
-		 	System.out.println();
-		for (int i = 1; i < 2; i++) {
-			System.out.println("Mushroom ");
-			System.out.println();
-			}
-			for (int j = 1; j < 10; j++) {
-				System.out.print("Badger ");
-			}
-			for (int y = 1; y < 10; y++) {
-				System.out.print("Badger ");
-			}
-			for (int z = 1; z < 2; z++) {
-				System.out.print("Mushroom ");
-			}
-			for(int r=1; r<10; r++) {
-				System.out.print("Badger" );
-			}
-			for (int e = 1; e < 1; e++) {
-				System.out.print("African! ");
-			}
-			for (int w = 1; w < 4; w++) {
-				System.out.println("Snake ");
-			}
-			for (int p = 1; p < 1; p++) {
-				System.out.print("Ooh it's a snake ");
-			}
-			for (int u = 1; u < 10; u++) {
-				System.out.println("Badger ");
-				{
-			}
-			for (int q = 1; q < 2; q++) {
-				System.out.print("Mushroom ");
-				}
-
-			}
-
+		System.out.println();
+		String part2 ="Mushroom,mushroom ";
+		for (int i = 1; i < 3; i++) {
+			System.out.print(part2);
 		}
-
+		System.out.println();
+		for (int j = 1; j < 11; j++) {
+			System.out.print(part1);
+		}
+		System.out.println();
+		for (int z = 1; z < 3; z++) {
+			System.out.print(part2);
+		}
+		System.out.println();
+		for (int r = 1; r < 11; r++) {
+			System.out.print(part1);
+		}
+		System.out.println();
+		String part3 ="Mush-mushroom";
+		for (int h = 1; h < 2; h++) {
+			System.out.print(part3);
+		}
+		System.out.println();
+		for (int r = 1; r < 11; r++) {
+			System.out.print(part1);
+		}
+		System.out.println();
+		String part4 ="African!";
+		for (int e = 1; e < 2; e++) {
+			System.out.print(part4);
+		}
+		String part5 ="Snake, a snake";
+		for (int w = 1; w < 2; w++) {
+			System.out.print(part5);
+		}
+		System.out.println();
+		String part6 ="Snake! A snake, Ooh it's a snake";
+		for (int p = 1; p < 2; p++) {
+			System.out.print(part6);
+		}
+		System.out.println();
+		for (int u = 1; u < 11; u++) {
+			System.out.print(part1);
+		}
+		System.out.println();
+		for (int q = 1; q < 3; q++) {
+			System.out.print(part2);
+		}
+		System.out.println();
 	}
 
-
+}
