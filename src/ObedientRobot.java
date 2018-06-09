6a1c6afe55224effedba7c237e30bdfa85268957
League <league@Mac-mini.local>
2018-06-09 11:29:37 -0700

    working on ObidientRobot
    Finished doing triangle. and square, working on making robot draw a circle.

--- src/ObedientRobot.java
+++ src/ObedientRobot.java
@@ -1,7 +1,40 @@
+import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
 	public static void main(String[] args) {
-	
+		Robot frost = new Robot();
+		frost.setSpeed(10);
+		frost.penDown();
+ 
+	//for (int i = 360; i <= 360; i--) {
+frost.turn(100);
+frost.move(100);
+frost.turn(100);
+frost.move(100);//this doesn't work!
+
+		//}
+
+}
+
+	public static void drawSquare(Robot robot, int size) {
+		robot.move(size);
+		robot.turn(90);
+		robot.move(size);
+		robot.turn(90);
+		robot.move(size);
+		robot.turn(90);
+		robot.move(size);
+		robot.penUp();
+
 	}
 
+	public static void drawTriangle(Robot robot, int size) {
+		robot.turn(-90);
+		robot.move(200);
+		robot.turn(120);
+		robot.move(200);
+		robot.turn(120);
+		robot.move(200);
+		robot.penUp();
+	}
 }
