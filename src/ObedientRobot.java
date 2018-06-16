11a3a445cd7ee4cef5216b68d40f5b3f1598fe64
tactreconx <league@WTS-MM-11.local>
2018-06-16 11:29:13 -0700

    Working on obedient robot

--- src/ObedientRobot.java
+++ src/ObedientRobot.java
@@ -1,3 +1,7 @@
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
@@ -5,18 +9,26 @@ public class ObedientRobot {
 		Robot frost = new Robot();
 		frost.setSpeed(10);
 		frost.penDown();
- 
-	//for (int i = 360; i <= 360; i--) {
-frost.turn(100);
-frost.move(100);
-frost.turn(100);
-frost.move(100);//this doesn't work!
+		String shapeEntered = JOptionPane.showInputDialog(null, "What shape do you want, circle, triangle, square");
+		if (shapeEntered.equalsIgnoreCase("Circle")) {
+			drawCircle(frost, 100);
+		}
+		else if(shapeEntered.equalsIgnoreCase("Square")) {
+			//drawSquare(frost,100);
+		}
+		else if(shapeEntered.equalsIgnoreCase("Triangle")) {
+			drawTriangle(frost,100); 
+		}
+		String shapeColor = JOptionPane.showInputDialog(null, "Between blue, red, and yellow, what color do you want?");
+		if (shapeColor.equalsIgnoreCase("blue")) {
+			//working on setting the color to blue
+			//remember color is a class Color.BLUE
+			// create a Color the same way you created a new Robot ex line 9 but include the color in the ().
+		}
+	}
 
-		//}
-
-}
-
-	public static void drawSquare(Robot robot, int size) {
+	public static void drawSquare(Robot robot, int size, Color color) {
+		robot.setPenColor(color);
 		robot.move(size);
 		robot.turn(90);
 		robot.move(size);
@@ -36,5 +48,12 @@ frost.move(100);//this doesn't work!
 		robot.turn(120);
 		robot.move(200);
 		robot.penUp();
+	}
+
+	public static void drawCircle(Robot robot, int size) {
+		for (int i = 1; i <= 360; i++) {
+			robot.turn(1);
+			robot.move(1);
+		}
 	}
 }
