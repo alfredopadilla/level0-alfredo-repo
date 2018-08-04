87c47465394e3bdf0345af11eff93e83c24df053
tactreconx <league@Admins-Mac-mini-2.local>
2018-08-04 11:32:24 -0700

    finished prime or not, still working on Fibonacci
    Fibonacci is almost done the only problem is when you run it the
    numbers are together ,1 and 2.

---
+++ src/Fibonacci.java
@@ -0,0 +1,16 @@
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 1;
+		System.out.print( "" + a +  " " + b );
+		for (int i = 0; i <= 12; i++) {
+			int c = a + b;
+			System.out.print( c + " ");
+			a = b;
+			b = c;
+		}
+	}
+
+}
--- src/PrimeorNot.java
+++ src/PrimeorNot.java
@@ -5,22 +5,25 @@ public class PrimeorNot {
 	public static void main(String[] args) {
 		String input = JOptionPane.showInputDialog(null, "Type in a number 1-100");
 		int value = Integer.parseInt(input);
-		boolean prime;
-		for (int i = 1; i < 100; i++) {
-			if (value > 1 && value % i == 1) {
-				prime = true;
-			} else {
-				prime = false;
+		if (value < 1 || value > 100) {
+			JOptionPane.showMessageDialog(null, "Error, please give a number between 1-100");
+		} else {
+
+			boolean prime = true;
+			for (int i = 2; i < value; i++) {
+				if (value > 1 && value % i == 0) {
+					prime = false;
+				}
+
 			}
+			if (prime == true) {
+				JOptionPane.showMessageDialog(null, "It's a prime number");
+			}
+			if (prime == false) {
+				JOptionPane.showMessageDialog(null, "It's not a prime number");
 
+			}
 		}
-		if (prime = true) {
-			JOptionPane.showMessageDialog(null, "It's a prime number");
-		}
-		if (prime = false) {
-			JOptionPane.showMessageDialog(null, "It's not a prime number");
 
-		}
-//mostly done something went wrong, aka when running it and input 24 it said its a prime when its's not 
 	}
 }
