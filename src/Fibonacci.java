4f0f66dce94e699746e2dfb43ee678dfd06fe1b1
Kendra Kleber <league@Mac-mini-2.local>
2018-08-11 11:23:00 -0700

    Finished Fibonacci and still working on Slippery Dot

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -4,10 +4,10 @@ public class Fibonacci {
 	public static void main(String[] args) {
 		int a = 1;
 		int b = 1;
-		System.out.print( "" + a +  " " + b );
+		System.out.print( a + " " + b +" ");
 		for (int i = 0; i <= 12; i++) {
 			int c = a + b;
-			System.out.print( c + " ");
+			System.out.print(c + " ");
 			a = b;
 			b = c;
 		}
