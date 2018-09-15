441c6bbd2a40622eddabd634ad7852910c7e21d7
alfredopadilla <league@WorthyMini-3.local>
2018-09-15 11:25:03 -0700

    Finished Last Summer and Where's Waldo

---
+++ src/LastSummer.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class LastSummer {
+
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog(null, "What's your name?");
+		JOptionPane.showMessageDialog(null, "I know you went swimming last summer, " + name + ". Muhahaha!");
+
+	}
+
+}
